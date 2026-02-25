d19a40860bb96878f578f55daf44cbb596171075
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2026-02-25 12:24:44 -0300

    liberando acesso a sites

--- api.py
+++ api.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 from src.utils import cleaning_text, predict_sentiment
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class Text(BaseModel):
     text: str
 
 
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # "*" significa: "Aceito pedidos de QUALQUER site"
+    allow_credentials=True,
+    allow_methods=["*"],  # Aceita POST, GET, etc.
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 def input_user(item: Text): 
