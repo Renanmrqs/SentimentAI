fce9897ec0cf7ce0ab6ea89606324191b442dd33
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2026-04-25 17:40:10 -0300

    # code review
    Added data preprocessing, model training, and saving functionality for sentiment analysis.

--- src/training.py
+++ src/training.py
@@ -9,6 +9,7 @@ import pickle
 df = pd.read_csv('data/IMDB_Cleaned.csv')
 print(f'review loaded: {len(df)}')
 
+# separando a review com o sentimento(sentimento contendo positivo ou negativo)
 x = df['review_cleaned']
 y = df['sentiment']
 print(x)
@@ -17,16 +18,20 @@ print(f'size x: {x.shape}')
 print(f'size y: {y.shape}')
 
 
-
+# separando delas em sentimento pra teste e treino, e review pra teste e treino
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 print(f'training: {len(x_train)} reviews')
 print(f'test: {len(x_test)} reviews')
+
+# vetorizando, (transfomando em numeros) com limite para n ficar muito pesado e demorar para treinar
 vectorizer = TfidfVectorizer(max_features=5000)
+# trasnformando em vetores as variavel de treinamento 
 x_train_tfidf = vectorizer.fit_transform(x_train)
 x_test_tfidf = vectorizer.transform(x_test)
+#limpando mais um pouco a variavel 
 print(f'vocabulary: {len(vectorizer.get_feature_names_out())} words')
 
-
+#escolhendo o modelo(regressao logisca e passando o maximo a ser utilizado)
 model = LogisticRegression(max_iter=1000)
 model.fit(x_train_tfidf, y_train)
 
@@ -34,12 +39,12 @@ model.fit(x_train_tfidf, y_train)
 print('testing model')
 y_pred = model.predict(x_test_tfidf)
 
-
+# fazendo o teste da porcentagem de acerto 
 accuracy = accuracy_score(y_test, y_pred)
 print(f'acuracy: {accuracy*100:.2f}%')
 print(classification_report(y_test, y_pred))
 
-
+# salvando em um arquivo serializavel
 with open('models/sentiment_model.pkl', 'wb') as f:
     pickle.dump(model, f)
 with open('models/vectorizer.pkl', 'wb') as f:
