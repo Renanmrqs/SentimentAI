0dc155623fce76e8dc2dcbc2996121958b6ddd8f
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2026-04-25 17:34:09 -0300

    explaining lines
    Added cleaned review column and saved to new CSV file.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,8 +10,10 @@ df = pd.read_csv('data/IMDB Dataset.csv')
 
 print(f'\nProcessando {len(df)} reviws...')
 
+# aplicando o texto limpo no dataset original e jogando pra uma variavel limpa
 df['review_cleaned'] = df['review'].apply(cleaning_text)
 
+# analise dos dados limpos
 for i in range(3):
     print(f'\n--- review {i+1} ---')
     print(f'normal {len(df.iloc[i]['review'].split())} palavras):')
@@ -19,5 +21,6 @@ for i in range(3):
     print(f'\nLimpa ({len(df.iloc[i]['review_cleaned'].split())} palavras):')
     print(df.iloc[i]['review_cleaned'][:200])
 
+# salvanco em um arquivo csv
 df.to_csv('data/IMDB_Cleaned.csv', index=False)
 print('salvo em: data/IMDB_Clened.csv')
